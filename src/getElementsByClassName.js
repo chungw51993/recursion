94aff3c70c65b77f1e562379833de4bbc476f63a
Jin Chung <chungw51993@gmail.com>
2017-01-26 22:12:23 -0600

    Check if body node contains className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,12 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var allElements = [];
+  var node = document.body || node;
+
+  if (node.classList && node.classList.contains(className)) {
+    allElements.push(node);
+  }
 };
