04fa1d03ac3ccdc3f6bdb99cf03cb308f598be12
Jin Chung <chungw51993@gmail.com>
2017-01-27 00:18:51 -0600

    Added function for object

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -8,74 +8,79 @@ var parseJSON = function(json) {
   var nextIndex;
 
   var nextChar = function() {
-    currentIndex +=;
+    currentIndex += 1;
     nextIndex = json.charAt(currentIndex);
     return nextIndex;
   };
 
-  var value = function() {
-    switch(nextIndex) {
-      case '{':
-        return object();
-      case '[':
-        return array();
-      case '\"':
-        return string();
-      case 't':
-      case 'f':
-        return boolean();
-      case 'n':
-        return nully();
-      default:
-        if(nextIndex === '-' || (nextIndex && nextIndex >= 0 && nextIndex <= 9)) {
-          return number();
-        } else {
-          error('bad JSON');
-        }
+  var error = function(message) {
+    console.log(message);
+    throw undefined;
+  };
+
+  var value = function () {
+  switch(nextIndex) {
+    case '{':
+      return object();
+    case '[':
+      return array();
+    case '\"':
+      return string();
+    case 't':
+    case 'f':
+      return bool();
+    case 'n':
+      return nully();
+    default:
+      if(nextIndex === '-' || (nextIndex && nextIndex >= 0 && nextIndex <= 9)) {
+        return number();
+      } else {
+        error('bad JSON');
+      }
       break;
     }
   };
 
   var nully = function() {
-    var nully = '';
-    if (nextIndex === 'n') {
-      for (var i = 0; i <= 4; i++) {
-        nully += nextIndex;
-        nextChar();
-      }
-      if (nully === 'null') {
-        return null;
-      } else {
-        error('bad null');
-      }
+  var nully = '';
+  if(nextIndex === 'n') {
+    _.times(4, function() {
+      nully += nextIndex;
+      nextChar();
+    });
+    if(nully === 'null') {
+      return null;
+    } else {
+      error('bad null');
     }
+  }
 
     error('bad null');
   };
 
-  var boolean = function() {
-    var bool = '';
-    if (nextIndex === 't') {
-      _.times(4, function() {
-        bool += nextIndex;
-        nextChar();
-      });
-      if (bool === 'true') {
-        return true;
-      } else {
-        error('bad bool');
-      }
-    } else if (nextIndex === 'f') {
-      _.times(5, function() {
-        bool += nextIndex;
-        nextChar();
-      });
-      if (bool === 'false') {
-        return false;
-      } else {
-        error('bad bool');
-      }
+  var bool = function() {
+  var bool = '';
+  if(nextIndex === 't') {
+    _.times(4, function() {
+      bool += nextIndex;
+      nextChar();
+    });
+    if(bool === 'true') {
+      return true;
+    } else {
+      error('bad bool');
     }
+  } else if(nextIndex === 'f') {
+    _.times(5, function() {
+      bool += nextIndex;
+      nextChar();
+    });
+    if(bool === 'false') {
+      return false;
+    } else {
+      error('bad bool');
+    }
+  }
 
     error('bad bool');
   };
@@ -83,74 +88,72 @@ var parseJSON = function(json) {
   var number = function() {
     var number = '';
     function getDigits() {
-      while (nextIndex && nextIndex >= 0 && nextIndex <= 9) {
+      while(nextIndex && nextIndex >= 0 && nextIndex <= 9) {
         number += nextIndex;
         nextChar();
       }
     }
 
-    if (nextIndex === '-') {
+    if(nextIndex === '-') {
       number += nextIndex;
       nextChar();
     }
 
     getDigits();
 
-    if (nextIndex === '.') {
+    if(nextIndex === '.') {
       number += nextIndex;
       nextChar();
       getDigits();
     }
 
-    if (nextIndex === 'e' || nextIndex === 'E') {
+    if(nextIndex === 'e' || nextIndex === 'E') {
       number += nextIndex;
       nextChar();
       if(nextIndex === '-' || nextIndex === '+') {
-        number +=nextIndex;
+        number += nextIndex;
         nextChar();
       }
-      getDigits();
+      getDigits(); // exponent
     }
 
-    if (!isNaN(Number(number))) {
+    if(!isNaN(Number(number))) { // check if string can be converted to number
       return Number(number);
-    } else {
+    } else { // string could not be converted to number
       error('bad number');
     }
   };
 
-  var escapes = {
-    'b': '\b',
-    'n': '\n',
-    't': '\t',
-    'r': '\r',
-    'f': '\f',
-    '\"': '\"',
-    '\\': '\\'
-  };
+  var escapes = { // helper variable
+  'b': '\b',
+  'n': '\n',
+  't': '\t',
+  'r': '\r',
+  'f': '\f',
+  '\"': '\"',
+  '\\': '\\'
+};
 
-  var string = function() {
-    var string = '';
-    if (nextIndex !== '\"') {
-      error('string should start with \"');
+var string = function() {
+  var string = '';
+  if(nextIndex !== '\"') error('string should start with \"');
+  nextChar();
+  while(nextIndex) {
+    if(nextIndex === '\"') {
+      nextChar();
+      return string;
     }
-    nextChar();
-    while(nextIndex) {
-      if (nextIndex === '\"') {
-        nextChar();
-        return string;
-      }
 
-      if (nextIndex === '\\') {
-        nextChar();
-        if (escapes.hasOwnProperty(nextIndex)) {
-          string += escapes[nextIndex];
-        } else {
-          string += nextIndex;
-        }
-      } else {
+    if(nextIndex === '\\') {
+      nextChar();
+      if(escapes.hasOwnProperty(nextIndex)) {
+        string += escapes[nextIndex];
+       } else {
         string += nextIndex;
       }
+    } else {
+        string += nextIndex;
+    }
       nextChar();
     }
     error('bad string');
@@ -158,21 +161,40 @@ var parseJSON = function(json) {
 
   var array = function() {
     var array = [];
-    if (nextIndex !== '[') error('array should start with [');
-    if (nextChar() === ']') {
-      return array;
-    }
+    if(nextIndex !== '[') error('array should start with [');
+    if(nextChar() === ']') return array;
 
     do {
       array.push(value());
-      if (nextIndex === ']') {
-        nextChar;
+      if(nextIndex === ']') {
+        nextChar();
         return array;
       }
-    } while (nextIndex && nextIndex === ',' && nextChar());
+    } while(nextIndex && nextIndex === ',' && nextChar());
 
     error('bad array');
   };
 
+  var object = function() {
+    var object = {};
+    if(nextIndex !== '{') error('object should start with {');
+    if(nextChar() === '}') return object;
 
+    do {
+      var key = string();
+      if(nextIndex !== ':') error('object property expecting ":"');
+      nextChar();
+      object[key] = value();
+      if(nextIndex === '}') {
+        nextChar();
+        return object;
+      }
+    } while(nextIndex && nextIndex === ',' && nextChar());
+
+    error('bad object');
+  };
+
+  at = 0;
+  ch = json.charAt(at);
+  return value();
 };
