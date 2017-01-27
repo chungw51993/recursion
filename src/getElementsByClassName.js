04fa1bdea0de976d31d27d23a38ed40b8baba908
Jin Chung <chungw51993@gmail.com>
2017-01-26 22:33:30 -0600

    Added checking childNode

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,12 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function(className, node) {
   // your code here
   var allElements = [];
-  var node = document.body || node;
+  node = node || document.body;
+  var childNodes = node.childNodes;
 
   if (node.classList && node.classList.contains(className)) {
     allElements.push(node);
   }
+
+  if (childNodes) {
+    for (var i = 0; i < childNodes.length; i++) {
+      allElements.push(getElementsByClassName(className,childNodes[i]));
+    }
+  }
+
+  return allElements;
 };
